76d30ceaa9db5faa204f39062bd197c83d04647d
ddpom <ddpom@outlook.com>
2019-12-23 18:27:27 +0800

    修改对命令的描述，使其更准确。

--- pullRepositories.sh
+++ pullRepositories.sh
@@ -1,11 +1,11 @@
-# open the directory
-# 打开 toolsRes 目录
+# Go to the repository directory
+# 转到仓库所在的目录
 cd ~/repo/toolsRes
 
 # Discard all the unstaged changes made to the repository.
 # 丢弃掉所有未暂存的文件的修改。
 git checkout .
 
-# git pull new commits to the repository with rebase option..
+# git pull new commits to the repository with rebase option.
 # 用带 rebase 选项的 git pull 更新这个仓库所有的新提交。
 git pull --rebase
