a5d57961ebf0ea9293f9e122e6e866475c42fbc6
sierxue <19348230+sierxue@users.noreply.github.com>
2020-01-12 22:08:21 +0800

    不更新 repo scripts，因为已经通过 submodule 放进 repo dotfiles-local 了。

--- pullDotfiles.sh
+++ pullDotfiles.sh
@@ -6,18 +6,6 @@ truncate -s 0 ~/.df/log/pullDotfiles.log
 
 # Go to the repository directory
 # 转到仓库所在的目录
-cd ~/.df/scripts
-
-# Discard all the unstaged changes made to the repository.
-# 丢弃掉所有未暂存的文件的修改。
-git checkout .
-
-# git pull new commits to the repository with rebase option.
-# 用带 rebase 选项的 git pull 更新这个仓库所有的新提交。
-git pull --rebase
-
-# Go to the repository directory
-# 转到仓库所在的目录
 cd ~/.df/dotfiles
 
 # Discard all the unstaged changes made to the repository.
