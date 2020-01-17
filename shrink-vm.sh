566f82416e53f12f795d6b19d44ee92aac3296e1
sierxue <19348230+sierxue@users.noreply.github.com>
2020-01-17 10:45:11 +0800

    在 shrink.sh 的基础上，增加清除 texlive 备份宏包。适用于虚拟机，因为虚拟机要求精简。

---
+++ shrink-vm.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Empty the Trash/Recycle Bin.
+cd .local/share/Trash/ && cd files && rm -rf *
+# Purge temporary files.
+sudo systemctl start systemd-tmpfiles-clean
+# Clean up APT cache in Ubuntu
+sudo apt clean
+# Get rid of packages (including old kernels) that are no longer required.
+sudo apt autoremove --purge
+# Clear back up for texlive packages.
+sudo tlmgr backup --clean --all
+
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
