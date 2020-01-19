353d1c1ea44de91b27a71e68034a0ef6c0536298
sierxue <19348230+sierxue@users.noreply.github.com>
2020-01-19 16:32:54 +0800

    pullRepo: Add two functions:     1. If repo toolsRes does not exist, pull from github;     2. Before clean toolsRes, backup the whole directory.

--- pullRepo.sh
+++ pullRepo.sh
@@ -1,19 +1,35 @@
 #!/usr/bin/env bash
 
-# Clear contents in ~/.df/log/pullRepo.log
-# 清除 ~/.df/log/pullRepo.log 文件中的内容。
-truncate -s 0 ~/.df/log/pullRepo.log
+# Clear contents in $HOME/.df/log/pullRepo.log
+# 清除 $HOME/.df/log/pullRepo.log 文件中的内容。
+truncate -s 0 $HOME/.df/log/pullRepo.log
+
+if [ ! -d $HOME/Documents/toolsRes ];
+then
+    git clone --recursive https://github.com/sierxue/toolsRes.git \
+    $HOME/Documents/toolsRes
+fi
+
+# Backup the repo to specified directory.
+# if [ ! -d $HOME/Documents/backup/toolsRes-backup ];
+# then
+#     rm -rf $HOME/Documents/backup/toolsRes-backup
+# fi
+if [ ! -d $HOME/Documents/backup ];
+then
+    mkdir $HOME/Documents/backup
+fi
+cp -r $HOME/Documents/toolsRes \
+$HOME/Documents/backup/toolsRes-backup-$(date +%Y-%m-%d-%H:%M)
 
 # Go to the repository directory
 # 转到仓库所在的目录
-cd ~/Documents/toolsRes
+cd $HOME/Documents/toolsRes
 
 # Discard all untracked files and the unstaged changes made to the repository.
 # 丢弃掉所有未追踪及未暂存的文件的修改。
 git clean -df
 git clean -dff
-git checkout -- elegantbook_sierxue.cls
-git checkout -- toolsRes.tex
 git checkout .
 
 # git pull new commits to the repository with rebase option.
